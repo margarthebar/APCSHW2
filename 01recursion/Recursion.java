ccc4ba849ac3431f98fac4134a645eaa349ba8c8
margarthebar <margarburkart@gmail.com>
2015-02-08 15:02:36 -0500

    added and tested fact(n)

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,12 +1,18 @@
 public class Recursion{
     public static String name(){
-	    return "Burkart,Margaret";
+	return "Burkart,Margaret";
     }
-    /*
+
     public static int fact(int n){
-
+	if(n<0){
+	    throw new IllegalArgumentException();
+	}else if(n==0){
+	    return 1;
+	}
+	return n * fact(n-1);
     }
 
+    /*
     public static int fib(int n){
 
     }
@@ -17,6 +23,18 @@ public class Recursion{
     */
 
     public static void main(String[]args){
+	System.out.println(name());
 
+	try{
+	    System.out.println(fact(5));
+	}catch(IllegalArgumentException e){
+	    System.out.println("Caught IllegalArgumentException");
+	}
+
+	try{
+	    System.out.println(fact(-3));
+	}catch(IllegalArgumentException e){
+	    System.out.println("Caught IllegalArgumentException");
+	}
     }
 }

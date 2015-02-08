c6b229f3cd070187e4fd84e3987c3dfe7218ee2d
margarthebar <margarburkart@gmail.com>
2015-02-08 15:56:26 -0500

    new testing

---
+++ 01recursion/demoRecursion.java
@@ -0,0 +1,43 @@
+public class demoRecursion.java{
+    public static void main(String[]args){
+	Recursion r = new Recursion();
+
+	System.out.println(r.name());
+
+	try{
+	    System.out.println(r.fact(5));
+	}catch(IllegalArgumentException e){
+	    System.out.println("Caught IllegalArgumentException");
+	}
+
+	try{
+	    System.out.println(r.fact(-3));
+	}catch(IllegalArgumentException e){
+	    System.out.println("Caught IllegalArgumentException");
+	}
+
+	try{
+	    System.out.println(r.fib(7));
+	}catch(IllegalArgumentException e){
+	    System.out.println("Caught IllegalArgumentException");
+	}
+
+	try{
+	    System.out.println(r.sqrt(25));
+	}catch(IllegalArgumentException e){
+	    System.out.println("Caught IllegalArgumentException");
+	}
+
+	try{
+	    System.out.println(r.sqrt(100));
+	}catch(IllegalArgumentException e){
+	    System.out.println("Caught IllegalArgumentException");
+	}
+
+	try{
+	    System.out.println(r.sqrt(-3));
+	}catch(IllegalArgumentException e){
+	    System.out.println("Caught IllegalArgumentException");
+	}
+    }
+}

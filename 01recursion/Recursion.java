2f5ca2d65878561f1998a7b9a2387457e4fea6b5
margarthebar <margarburkart@gmail.com>
2015-02-08 14:08:21 -0500

    added name()

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -1,3 +1,22 @@
 public class Recursion{
+    public static String name(){
+	    return "Burkart,Margaret";
+    }
+    /*
+    public static int fact(int n){
 
+    }
+
+    public static int fib(int n){
+
+    }
+
+    public static double sqrt(double n){
+
+    }
+    */
+
+    public static void main(String[]args){
+
+    }
 }

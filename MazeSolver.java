3ab2d09bf4b7b1ec5c12953656d52a6fbc951c81
margarthebar <margarburkart@gmail.com>
2015-02-09 22:18:25 -0500

    added mazeSolve and mazeHelp (untested)

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,6 +1,20 @@
 public class MazeSolver{
-    public static char[][] mazeSolve(char[] maze){
-	
+    public static char[][] mazeSolve(char[][] maze){
+	int x,y;
+	char[][] copy = new char[maze.length][maze[0].length];
+	for(int r=0; r<maze.length ; r++){
+	    for(int c=0; c<maze[r].length ; c++){
+		copy[r][c] = maze[r][c];
+		if(maze[r][c]=='S'){
+		    x = r;
+		    y = c;
+		}
+	    }
+	}
+	return mazeHelp(copy,x,y);
+    }
+    public static char[][] mazeHelp(char[][] maze, int x, int y){
+	return maze;
     }
 
     public static void main(String[]args){

6dbad1483d14361c3c470cafeb06a62402c34a21
margarthebar <margarburkart@gmail.com>
2015-02-27 13:07:16 -0500

    editted merge method

--- 05MergeSort/MergeSort.java
+++ 05MergeSort/MergeSort.java
@@ -1,13 +1,15 @@
 public class MergeSort{
     public static int[] merge(int[] a, int[] b){
 	int[] c = new int[a.length+b.length];
-	merge(a,b,c,0);
+	merge(a,b,c,0,0,0);
     }
-    public static int[] merge(int[] a, int[] b, int[] c, int index){
+    public static int[] merge(int[] a, int[] b, int[] c, int aIndex, int bIndex, int cIndex){
 	if(a[0]<=b[0]){
-	    c[index]=a[0];
+	    c[cIndex]=a[aIndex];
+	    merge(a,b,c,aIndex+1,bIndex,cIndex+1);
 	}else{
-	    c[index]=b[0];
+	    c[cIndex]=b[bIndex];
+	    merge(a,b,c,aIndex,bIndex+1,cIndex+1);
 	}
     }
 }

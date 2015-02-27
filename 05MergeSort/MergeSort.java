c2dca6c422569b7d39a1d6a45cec28839cfef66d
margarthebar <margarburkart@gmail.com>
2015-02-27 13:17:15 -0500

    added return statements

--- 05MergeSort/MergeSort.java
+++ 05MergeSort/MergeSort.java
@@ -1,7 +1,7 @@
 public class MergeSort{
     public static int[] merge(int[] a, int[] b){
 	int[] c = new int[a.length+b.length];
-	merge(a,b,c,0,0,0);
+	return merge(a,b,c,0,0,0);
     }
     public static int[] merge(int[] a, int[] b, int[] c, int aIndex, int bIndex, int cIndex){
 	//if finished merging
@@ -11,19 +11,19 @@ public class MergeSort{
 	//if still b elements left over
 	if(aIndex==a.length){
 	    c[cIndex]=b[bIndex];
-	    merge(a,b,c,aIndex,bIndex+1,cIndex+1);
+	    return merge(a,b,c,aIndex,bIndex+1,cIndex+1);
 	}
 	//if still a elements left over
 	if(bIndex==b.length){
 	    c[cIndex]=a[aIndex];
-	    merge(a,b,c,aIndex+1,bIndex,cIndex+1);
+	    return merge(a,b,c,aIndex+1,bIndex,cIndex+1);
 	}
 	if(a[0]<=b[0]){
 	    c[cIndex]=a[aIndex];
-	    merge(a,b,c,aIndex+1,bIndex,cIndex+1);
+	    return merge(a,b,c,aIndex+1,bIndex,cIndex+1);
 	}else{
 	    c[cIndex]=b[bIndex];
-	    merge(a,b,c,aIndex,bIndex+1,cIndex+1);
+	    return merge(a,b,c,aIndex,bIndex+1,cIndex+1);
 	}
     }
     public static void main(String[]args){

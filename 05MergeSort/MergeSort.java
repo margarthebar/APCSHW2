aceaaf458d655b41c8b16add29e82f179d365663
margarthebar <margarburkart@gmail.com>
2015-02-27 12:58:38 -0500

    added basic merge idea

--- 05MergeSort/MergeSort.java
+++ 05MergeSort/MergeSort.java
@@ -1,3 +1,13 @@
 public class MergeSort{
-
+    public static int[] merge(int[] a, int[] b){
+	int[] c = new int[a.length+b.length];
+	merge(a,b,c,0);
+    }
+    public static int[] merge(int[] a, int[] b, int[] c, int index){
+	if(a[0]<=b[0]){
+	    c[index]=a[0];
+	}else{
+	    c[index]=b[0];
+	}
+    }
 }

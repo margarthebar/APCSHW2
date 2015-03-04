35cd37b505f3235d19c43867642ff29e8cb869ef
margarthebar <margarburkart@gmail.com>
2015-03-03 21:25:39 -0500

    ctravel constructor

--- 04USACO/ctravel.java
+++ 04USACO/ctravel.java
@@ -1,5 +1,20 @@
 public class ctravel{
+    public int[][] land;
+    public int seconds;
+    public String[] original;
+    public String[] coordinates;
+
     public ctravel(String input){
-	String fileContents = "4 5 6\n...*.\n...*.\n.....\n.....\n1 3 1 5";
+	String[] in = input.split("\n");
+	original = in[0].split(" ");
+	land = new int[Integer.parseInt(original[0])][Integer.parseInt(original[1])];
+	seconds = Integer.parseInt(original[2]);
+	coordinates = in[in.length-1].split(" ");
+	for(int i=1; i<in.length-1; i++){
+	    String[] line = in[i].split(" ");
+	    for(int c=0; c<land[0].length; c++){
+		land[i-1][c] = Integer.parseInt(line[i-1]);
+	    }
+	}
     }
 }

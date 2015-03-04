13dfd4cf9a0a39effe2de3ec928cb2ee5a27503c
margarthebar <margarburkart@gmail.com>
2015-03-03 21:41:52 -0500

    working constructor

--- 04USACO/ctravel.java
+++ 04USACO/ctravel.java
@@ -1,5 +1,5 @@
 public class ctravel{
-    public int[][] land;
+    public String[][] land;
     public int seconds;
     public String[] original;
     public String[] coordinates;
@@ -7,14 +7,25 @@ public class ctravel{
     public ctravel(String input){
 	String[] in = input.split("\n");
 	original = in[0].split(" ");
-	land = new int[Integer.parseInt(original[0])][Integer.parseInt(original[1])];
+	land = new String[Integer.parseInt(original[0])][Integer.parseInt(original[1])];
 	seconds = Integer.parseInt(original[2]);
 	coordinates = in[in.length-1].split(" ");
+	System.out.println(this);
 	for(int i=1; i<in.length-1; i++){
-	    String[] line = in[i].split(" ");
 	    for(int c=0; c<land[0].length; c++){
-		land[i-1][c] = Integer.parseInt(line[i-1]);
+		land[i-1][c] = in[i].substring(c,c+1);
 	    }
 	}
     }
+    public String toString(){
+	String res = "";
+	for(int i=0; i<land.length; i++){
+	    for(int j=0; j<land[0].length; j++){
+		res += land[i][j] + " ";
+	    }
+	    res += "\n";
+	}
+	return res;
+
+    } 
 }
--- 04USACO/ctravelDriver.java
+++ 04USACO/ctravelDriver.java
@@ -1,5 +1,7 @@
 public class ctravelDriver{
     public static void main(String[]args){
-		String fileContents = "4 5 6\n...*.\n...*.\n.....\n.....\n1 3 1 5";
+	String fileContents = "4 5 6\n...*.\n...*.\n.....\n.....\n1 3 1 5";
+	ctravel field = new ctravel(fileContents);
+	System.out.println(field);
     }
 }

8e7e6fcb052c32b3c4a926affef465e16f310a51
margarthebar <margarburkart@gmail.com>
2015-03-03 23:06:33 -0500

    updated format

---
+++ 05MergeSort/Sorts.java
@@ -0,0 +1,179 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Random;
+public class Sorts{
+    //extra
+    public static String name(){
+	return "Burkart,Margaret";
+    }
+    public static int period(){
+	return 6;
+    }
+    public static void insertionSort(int[]c){
+	int x = 0;
+	//test for each element in array
+	for(int i=1;i<c.length;i++){
+	    //compare to each element before it in the array
+	    for(int j=0;j<i;j++){
+		//if c[i]<c[j]
+		if(c[i]<c[j]){
+		    //store value to be moved towards the front
+		    x = c[i];
+		    //now shift everything over (k is just j)
+		    for(int k=i;k>j;k--){
+			c[k] = c[k-1];
+		    }
+		    //now add x to the vacant spot
+		    c[j] = x;
+		}
+	    }
+	}
+    }
+
+    public static void selectionSort(int[] c){
+	int smallest = c[0];
+	for(int i=0; i<c.length; i++){
+	    smallest = c[i];
+	    for(int j=i; j<c.length; j++){
+		if(c[j]<smallest){
+		    smallest = c[j];
+		    c[j] = c[i];
+		    c[i] = smallest;
+		}
+	    }
+	}
+    }
+
+    public static void bubbleSort(int[]c){
+	int temp = 0;
+	for(int upper=c.length; upper>1; upper--){
+	    for(int i=0; i<upper-1; i++){
+		if(c[i+1]<c[i]){
+		    temp = c[i];
+		    c[i] = c[i+1];
+		    c[i+1] = temp;
+		}
+	    }
+	}
+    }
+
+     public static void radixSort(int[]c){
+	int place = 10;
+	ArrayList<ArrayList<Integer>> bucketArray = new ArrayList<ArrayList<Integer>>(10);
+	//intialize buckets
+	for(int i=0;i<10;i++){
+	    bucketArray.add(new ArrayList<Integer>());
+	}
+	//find the necessary number of passes through the original array
+	int digits = 1;
+	for(int a:c){
+	    if((""+a).length()>digits){
+		digits = (""+a).length();
+	    }   
+	}
+	//sort into buckets
+	for(int i=0; i<digits; i++){
+	    for(int a:c){
+		bucketArray.get((a%place-a%(place/10))/(place/10)).add(a);
+	    }
+	    place*=10;
+	    //copy values in order over to original array
+	    int indexOriginal = 0;
+	    for(ArrayList<Integer> a:bucketArray){
+		for(int el:a){
+		    c[indexOriginal]=el;
+		    indexOriginal++;
+		}
+		//erase values in bucketArray for reuse
+		a.clear();
+	    }
+	}
+    }
+     public static int[] mergeSort(int[] a){
+	//base case: one element
+	if(a.length==1){
+	    return a;
+	}
+	//if more than one element
+	int[] x = new int[a.length/2];
+	int[] y = new int[a.length/2];
+	for(int i=0; i<a.length/2; i++){
+	    x[i] = a[i];
+	}
+	for(int j=0; j<a.length/2; j++){
+	    y[j] = a[a.length/2+j];
+	}
+	return merge(mergeSort(x),mergeSort(y));
+    }
+
+    public static int[] merge(int[] a, int[] b){
+	int[] c = new int[a.length+b.length];
+        int aIndex = 0;
+	int bIndex = 0;
+	int cIndex = 0;
+	while(aIndex<a.length && bIndex<b.length){
+	    if(a[aIndex]<=b[bIndex]){
+		c[cIndex] = a[aIndex];
+		aIndex+=1;
+	    }else{
+		c[cIndex] = b[bIndex];
+		bIndex+=1;
+	    }
+	    cIndex+=1;
+	}
+	if(aIndex<a.length){
+	    while(aIndex<a.length){
+		c[cIndex] = a[aIndex];
+		aIndex+=1;
+		cIndex+=1;
+	    }
+	}
+	if(bIndex<b.length){
+	    while(bIndex<b.length){
+		c[cIndex] = b[bIndex];
+		bIndex+=1;
+		cIndex+=1;
+	    }
+	}
+	return c;
+    }
+
+    public static int[] randomArray(){
+	Random rand = new Random();
+	int[] ans = new int[1000];
+	for(int i=0;i<1000;i++){
+	    ans[i] = rand.nextInt();
+	    if(ans[i]<0){
+		ans[i]*=-1;
+	    }
+	}
+	return ans;
+    }
+
+    public static void main(String[]args){
+	long startTime,endTime;
+	int[] a = randomArray();
+	startTime = System.currentTimeMillis();
+	bubbleSort(a);
+	endTime = System.currentTimeMillis();
+	System.out.println("bubbleSort: "+(endTime-startTime));
+
+	int[] b = randomArray();
+	startTime = System.currentTimeMillis();
+	insertionSort(b);
+	endTime = System.currentTimeMillis();
+	System.out.println("insertionSort: "+(endTime-startTime));
+
+	int[] c = randomArray();
+	startTime = System.currentTimeMillis();
+	selectionSort(c);
+	endTime = System.currentTimeMillis();
+	System.out.println("selectionSort: "+(endTime-startTime));
+
+	int[] d = randomArray();
+	startTime = System.currentTimeMillis();
+	radixSort(d);
+	endTime = System.currentTimeMillis();
+	System.out.println("radixSort: "+(endTime-startTime));
+    }
+}

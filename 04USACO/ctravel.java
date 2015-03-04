4d6ca3073353e8560852197822d64c173aeea33d
margarthebar <margarburkart@gmail.com>
2015-03-03 22:16:30 -0500

    pathFind written

--- 04USACO/ctravel.java
+++ 04USACO/ctravel.java
@@ -1,8 +1,10 @@
+import java.lang.Math;
 public class ctravel{
     public String[][] land;
     public int seconds;
     public String[] original;
     public String[] coordinates;
+    public int paths = 0;
 
     public ctravel(String input){
 	String[] in = input.split("\n");
@@ -10,7 +12,6 @@ public class ctravel{
 	land = new String[Integer.parseInt(original[0])][Integer.parseInt(original[1])];
 	seconds = Integer.parseInt(original[2]);
 	coordinates = in[in.length-1].split(" ");
-	System.out.println(this);
 	for(int i=1; i<in.length-1; i++){
 	    for(int c=0; c<land[0].length; c++){
 		land[i-1][c] = in[i].substring(c,c+1);
@@ -18,10 +19,36 @@ public class ctravel{
 	}
     }
     public int pathFind(){
-	return pathFind(coordinates[0],coordinates[1],coordinates[2],coordinates[3]);
+	pathFind(Integer.parseInt(coordinates[0]),Integer.parseInt(coordinates[1]),Integer.parseInt(coordinates[2]),Integer.parseInt(coordinates[3]),seconds);
+	return paths;
     }
-    public int pathfind(int r1, int c1, int r2, int c2){
-
+    public boolean pathFind(int r1, int c1, int r2, int c2, int sec){
+	//base case: if off land
+	if(r1<0 || r1>=land.length || c1<0 || c1>=land[0].length){
+	    return false;
+	}
+	//base case: if moves gone
+	if(sec==0){
+	    if(r1==r2 && c1==c2){
+		paths+=1;
+		return false;
+	    }
+	    return false;
+	}
+	//base case: if solution not possible
+	if(Math.abs(r1-r2)+Math.abs(c1-c2)>sec){
+	    return false;
+	}
+	//base case: if *
+	if(land[r1][r2].equals("*")){
+	    return false;
+	}
+	//else
+	pathFind(r1,c1+1,r2,c2,sec-1);
+	pathFind(r1,c1-1,r2,c2,sec-1);
+	pathFind(r1+1,c1,r2,c2,sec-1);
+	pathFind(r1-1,c1,r2,c2,sec-1);
+	return false;
     }
 
     public String toString(){

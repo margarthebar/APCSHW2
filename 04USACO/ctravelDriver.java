4910ae420706a30e46b8e13c7bea69ba7a262fe0
margarthebar <margarburkart@gmail.com>
2015-03-03 22:47:03 -0500

    testing

--- 04USACO/ctravelDriver.java
+++ 04USACO/ctravelDriver.java
@@ -4,5 +4,20 @@ public class ctravelDriver{
 	ctravel field = new ctravel(fileContents);
 	System.out.println(field);
 	System.out.println(field.pathFind());
+
+	String fileContents1 = "3 3 4\n...\n...\n...\n1 1 3 3";
+	ctravel field1 = new ctravel(fileContents1);
+	System.out.println(field1);
+	System.out.println(field1.pathFind());
+
+	String fileContents2 = "5 5 14\n....*\n.....\n..*..\n.....\n*....\n1 1 5 5";
+	ctravel field2 = new ctravel(fileContents2);
+	System.out.println(field2);
+	System.out.println(field2.pathFind());
+
+	String fileContents3 = "7 7 22\n....*..\n.*.....\n..*...*\n.......\n*...*..\n.*.....\n....*..\n1 1 7 7";
+	ctravel field3 = new ctravel(fileContents3);
+	System.out.println(field3);
+	System.out.println(field3.pathFind());
     }
 }

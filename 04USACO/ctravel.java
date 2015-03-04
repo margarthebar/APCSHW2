6988809fe266f5bea113ba15de18a624d0f7db67
margarthebar <margarburkart@gmail.com>
2015-03-03 21:13:06 -0500

    created ctravel

---
+++ 04USACO/ctravel.java
@@ -0,0 +1,5 @@
+public class ctravel{
+    public ctravel(String input){
+	String fileContents = "4 5 6\n...*.\n...*.\n.....\n.....\n1 3 1 5";
+    }
+}
---
+++ 04USACO/ctravelDriver.java
@@ -0,0 +1,5 @@
+public class ctravelDriver{
+    public static void main(String[]args){
+
+    }
+}

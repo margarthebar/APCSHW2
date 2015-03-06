a337470957fbaa669203ba62d9ce6b8e44b5ca6a
margarthebar <margarburkart@gmail.com>
2015-03-05 21:18:54 -0500

    finished partition

--- quickSelect.java
+++ quickSelect.java
@@ -8,7 +8,8 @@ public class quickSelect{
 	    d[i]=ary[si];
 	}
 	int pivot = ary[si];
-	for(int i=si; i<ei+1; i++){
+	int end = ei+1;
+	for(int i=si+1; i<end; i++){
 	    if(ary[i]<pivot){
 		d[si] = ary[i];
 		si++;
@@ -18,12 +19,23 @@ public class quickSelect{
 		ei--;
 	    }
 	}
-	ary[si] = pivot;
+	d[si] = pivot;
+	System.out.println(toString(d));
+    }
+    public static String toString(int[] a){
+	String ans = "{";
+	for(int i=0; i<a.length; i++){
+	    ans+=a[i];
+	    if(i!=a.length-1){
+		ans+=",";
+	    }
+	}
+	ans+="}";
+	return ans;
     }
     public static void main(String[] args){
 	int[] a = {71,29,36,8,14,1,9,7,91,83,20};
-	System.out.println(a.toString());
+	System.out.println(toString(a));
 	partition(a,0,10);
-	System.out.println(a.toString());
     }
 }

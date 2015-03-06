a96b1fc74759ab363cf898cc2e3f8512b63a99af
margarthebar <margarburkart@gmail.com>
2015-03-05 20:43:59 -0500

    added quickSelect

---
+++ quickSelect.java
@@ -0,0 +1,11 @@
+public class quickSelect{
+    public static void partition(int[] ary, int si, int ei){
+	int[] d = new int[ary.length];
+	for(int i=0; i<si; i++){
+	    d[i]=ary[i];
+	}
+	for(int i=ei+1; i<ary.length; i++){
+	    d[i]=ary[i];
+	}
+    }
+}

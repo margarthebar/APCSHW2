3d3e4951e866cd1d6e6842f4ac9c6c84b4220004
margarthebar <margarburkart@gmail.com>
2015-03-05 20:54:36 -0500

    finished draft of partition

--- quickSelect.java
+++ quickSelect.java
@@ -5,7 +5,19 @@ public class quickSelect{
 	    d[i]=ary[i];
 	}
 	for(int i=ei+1; i<ary.length; i++){
-	    d[i]=ary[i];
+	    d[i]=ary[si];
 	}
+	int pivot = ary[si];
+	for(int i=si; i<ei+1; i+=){
+	    if(ary[i]<pivot){
+		d[si] = ary[i];
+		si++;
+	    }
+	    if(ary[i]>pivot){
+		d[ei] = ary[i];
+		ei--;
+	    }
+	}
+	ary[si] = pivot;
     }
 }

1a106c31f7acafc643543c2098e1fcdd89d34dc1
margarthebar <margarburkart@gmail.com>
2015-03-10 20:46:38 -0400

    LNode done

--- 07LinkedLists/LNode.java
+++ 07LinkedLists/LNode.java
@@ -2,9 +2,19 @@ public class LNode{
     private int value;
     private LNode next;
 
-    public getNext(){}
-    public getValue(){}
-    public setNext(){}
-    public setValue(){}
-    public toString(){}
+    public LNode getNext(){
+	return next;
+    }
+    public int getValue(){
+	return value;
+    }
+    public void setNext(LNode nxt){
+	next = nxt;
+    }
+    public int setValue(int n;){
+	value = n;
+    }
+    public String toString(){
+	return value;
+    }
 }

5760f14a83451a5f8d61ca52c82078d6469e4bd3
margarthebar <margarburkart@gmail.com>
2015-03-10 20:44:41 -0400

    barebones

---
+++ 07LinkedLists/LNode.java
@@ -0,0 +1,10 @@
+public class LNode{
+    private int value;
+    private LNode next;
+
+    public getNext(){}
+    public getValue(){}
+    public setNext(){}
+    public setValue(){}
+    public toString(){}
+}

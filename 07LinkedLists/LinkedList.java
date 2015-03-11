41a8a965e3a7e8858d46c80a28ad4d8913b9a066
margarthebar <margarburkart@gmail.com>
2015-03-10 21:00:00 -0400

    barebones

--- 07LinkedLists/LinkedList.java
+++ 07LinkedLists/LinkedList.java
@@ -1,3 +1,26 @@
 public class LinkedList{
+    public int get(int index){
 
+    }
+    public void set(int index, int n){
+
+    }
+    public int size(){
+	return 42;
+    }
+    public int indexOf(int value){
+	return int;
+    }
+    public void remove(int index){
+
+    }
+    public void add(int value){
+
+    }
+    public void add(int value, int index){
+
+    }
+    public String toString(){
+
+    }
 }

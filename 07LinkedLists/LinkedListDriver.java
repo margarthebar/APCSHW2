a54438182ca0d6a62fa50dfd461f8d0899955814
margarthebar <margarburkart@gmail.com>
2015-03-11 18:45:16 -0400

    created driver

---
+++ 07LinkedLists/LinkedListDriver.java
@@ -0,0 +1,12 @@
+public class LinkedListDriver{
+    public static void main(String[]args){
+	LinkedList s = new LinkedList();
+	System.out.println(s.toString());
+	s.add(3);
+	System.out.println(s.toString());
+	s.add(2);
+	s.add(9);
+	s.add(0);
+	System.out.println(s.toString());
+    }
+}

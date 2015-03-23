aead3e8fdc16bb147b24a9b77d9ccb852f9c2829
margarthebar <margarburkart@gmail.com>
2015-03-23 18:57:55 -0400

    tested everything

--- 09MyStack/MyQueueDriver.java
+++ 09MyStack/MyQueueDriver.java
@@ -1,9 +1,18 @@
 public class MyQueueDriver{
     public static void main(String[]args){
 	MyQueue<Integer> q = new MyQueue<Integer>();
+	System.out.println(q.toString());
 	q.enqueue(0);
+	System.out.println(q.toString());
 	q.enqueue(3);
+	System.out.println(q.toString());
 	q.enqueue(7);
+	System.out.println(q.toString());
 	System.out.println(q.dequeue());
+	System.out.println(q.toString());
+	System.out.println(q.dequeue());
+	System.out.println(q.toString());
+	System.out.println(q.dequeue());
+	System.out.println(q.toString());
     }
 }

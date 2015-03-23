9ec53ea448a168720e49a2fa06773ef1878a7d6b
margarthebar <margarburkart@gmail.com>
2015-03-23 18:56:29 -0400

    added toString()

--- 09MyStack/MyQueue.java
+++ 09MyStack/MyQueue.java
@@ -3,12 +3,16 @@ public class MyQueue<T>{
     private MyLinkedList<T> line = new MyLinkedList<T>();
 
     public boolean enqueue(T n){
-    line.add(n);
-    return true;
+	line.add(n);
+	return true;
     }
     public T dequeue(){
-    T ans = line.get(0);
-    line.remove(0);
-    return ans;
+	T ans = line.get(0);
+	line.remove(0);
+	return ans;
+    }
+
+    public String toString(){
+	return line.toString();
     }
 }

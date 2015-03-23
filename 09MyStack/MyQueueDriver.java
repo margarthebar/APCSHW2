f3355728015a4cb26657a1714c3e79267453db17
margarthebar <margarburkart@gmail.com>
2015-03-23 18:53:06 -0400

    enqueue and dequeue work

--- 09MyStack/MyQueueDriver.java
+++ 09MyStack/MyQueueDriver.java
@@ -1,5 +1,9 @@
 public class MyQueueDriver{
     public static void main(String[]args){
-	MyQueue<Integer> T = new MyQueue<Integer>();
+	MyQueue<Integer> q = new MyQueue<Integer>();
+	q.enqueue(0);
+	q.enqueue(3);
+	q.enqueue(7);
+	System.out.println(q.dequeue());
     }
 }

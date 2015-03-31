e9562e4b124920232efe5d6061e3188109af92ed
Margaret Burkart <margarburkart@gmail.com>
2015-03-31 12:55:06 -0400

    added framework

---
+++ 11BFS/Maze.java
@@ -0,0 +1,35 @@
+public class Maze{
+    private static final String clear =  "\033[2J";
+    private static final String hide =  "\033[?25l";
+    private static final String show =  "\033[?25h";
+    private String go(int x,int y){
+	return ("\033[" + x + ";" + y + "H");
+    }
+
+    /** Same constructor as before...*/
+    public Maze(String filename){     }
+
+    public String toString();//do not do the funky character codes
+
+    public String toString(boolean animate); //do the funky character codes when animate is true
+
+    /**Solve the maze using a frontier in a BFS manner. 
+     * When animate is true, print the board at each step of the algorithm.
+     * Replace spaces with x's as you traverse the maze. 
+     */
+    public boolean solveBFS(boolean animate){    }
+
+    /**Solve the maze using a frontier in a DFS manner. 
+     * When animate is true, print the board at each step of the algorithm.
+     * Replace spaces with x's as you traverse the maze. 
+     */
+    public boolean solveDFS(boolean animate){    }
+
+    public boolean solveBFS(){
+	return solveBFS(false);
+    }
+    public boolean solveDFS(){
+	return solveDFS(false);
+    }
+}
+

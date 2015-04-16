7d0e7a41a2e629ceb1683b8f6f113105e48ab3bd
margarthebar <margarburkart@gmail.com>
2015-04-15 22:42:00 -0400

    simplified Frontier and Maze

--- 11BFS/Frontier.java
+++ 11BFS/Frontier.java
@@ -1,70 +1,54 @@
 public class Frontier{
-    private LNodeBack<Coordinate> start;
+    private Coordinate start;
     private int mode = -1;
+    private MyDeque<Coordinate> moves = new MyDeque<Coordinate>();
     //mode 0 is BFS, mode 1 is DFS
+    public String toString(){
+	return moves.toString();
+    }
     public Frontier(){
 	this(-1);
     }
     public Frontier(int mode){
         this.mode = mode;
     }
-    public Frontier(LNodeBack<Coordinate> st){
-	setStart(st);
-	moves.addLast(st);
+    public Coordinate getStart(){
+	return start;
     }
-
+    public void setStart(Coordinate st){
+	start = st;
+    }
     public void add(Coordinate cor){
-	LNodeBack<Coordinate> node = new LNodeBack<Coordinate>(cor);
 	if(moves.size()==0){
-	    setStart(node);
+	    setStart(cor);
 	}
 	if(mode==0){
-	    moves.addLast(node);
+	    moves.addLast(cor);
 	}else if(mode==1){
-	    moves.addLast(node);
+	    moves.addLast(cor);
 
 	}
     }
     public Coordinate remove(){
 	Coordinate removed = new Coordinate(0,0);
 	if(mode==0){
-	    removed = moves.removeFirst().getValue();
+	    removed = moves.removeFirst();
 	}else if(mode==1){
-	    removed = moves.removeLast().getValue();
+	    removed = moves.removeLast();
 	}
 	return removed;
     }
     public boolean hasNext(){
 	return moves.size()!=0;
     }
-
-    public LNodeBack<Coordinate> getStart(){
-	return start;
-    }
-    public void setStart(LNodeBack<Coordinate> st){
-	start = st;
-    }
-
-    private MyDeque<LNodeBack<Coordinate>> moves = new MyDeque<LNodeBack<Coordinate>>();
-    
-    public String toString(){
-	return moves.toString();
-    }
-    public void addMove(LNodeBack<Coordinate> cor){
-	moves.addLast(cor);
-    }
-    public LNodeBack<Coordinate> removeMove(){
-	return moves.removeFirst();
-    }
-    public LNodeBack<Coordinate> removeLast(){
-	return moves.removeLast();
-    }
-    public LNodeBack<Coordinate> getFirst(){
+    public Coordinate getFirst(){
 	return moves.getFirst();
     }
-    public LNodeBack<Coordinate> getLast(){
+    public Coordinate getLast(){
 	return moves.getLast();
     }
+    //////////////////////
+    /*
     public String printPath(){
 	String ans = "[";
 	MyStack<Coordinate> path = getLast().pathFind();
@@ -75,4 +59,5 @@ public class Frontier{
 	ans+="]";
 	return ans;
     }
+    */
 }

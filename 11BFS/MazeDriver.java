2e55ee0ca0bd955fbe7f80b616df5827bd6f56c7
margarthebar <margarburkart@gmail.com>
2015-04-15 21:28:29 -0400

    solveBFS tested and found to be sound

--- 11BFS/MazeDriver.java
+++ 11BFS/MazeDriver.java
@@ -1,9 +1,16 @@
-import java.util.*;
-import java.io.*;
 public class MazeDriver{
     public static void main(String[]args){
+	/*
+	Maze m = new Maze("sampleMaze2.txt");
+	System.out.println(m.toString());
+	m.solveBFS(true);
+
+	Maze n = new Maze("sampleMaze.txt");
+	System.out.println(n.toString());
+	n.solveBFS();
+	*/
 	Maze m2 = new Maze("sampleMaze2.txt");
 	System.out.println(m2.toString());
-	m2.solveDFS(true);
+	m2.solveBFS(true);
     }
 }

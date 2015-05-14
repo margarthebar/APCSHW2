f179bab845e91ddced9dba6d41727118d43051be
Margaret Burkart <margarburkart@gmail.com>
2015-05-14 12:47:51 -0400

    skeleton getMedian

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,4 +1,21 @@
 public class RunningMedian{
     public MyHeap smaller = new MyHeap(true);
     public MyHeap larger = new MyHeap(false);
+
+    public void add(int value){
+	
+    }
+    public double getMedian(){
+	if(smaller.peek()==null && larger.peek()==null){
+	    return null;
+	}else if(smaller.peek()==null){
+	    return (double)larger.peek();
+	}else if(larger.peek()==null){
+	    return (double)smaller.peek();
+	}else{
+	    //if more in smaller
+	    //if more in larger
+	    //if same size
+	}
+    }
 }
---
+++ 14RunningMedian/RunningMedian.java~
@@ -0,0 +1,4 @@
+public class RunningMedian{
+    public MyHeap smaller = new MyHeap(true);
+    public MyHeap larger = new MyHeap(false);
+}

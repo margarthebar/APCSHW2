d8917f80c8a060e298772ff9b499e789a3ebe82e
margarthebar <margarburkart@gmail.com>
2015-05-16 13:16:22 -0400

    fixed remove

--- 13MyHeap/MyHeapDriver.java
+++ 13MyHeap/MyHeapDriver.java
@@ -2,20 +2,42 @@ public class MyHeapDriver{
     public static void main(String[]args){
 	MyHeap h = new MyHeap();
         h.add(16);
-	//System.out.println(h.toString());
+	System.out.println(h.toString());
+	System.out.println();
 	h.add(32);
-	//System.out.println(h.toString());
+	System.out.println(h.toString());
+	System.out.println();
 	h.add(10);
-	//System.out.println(h.toString());
+	System.out.println(h.toString());
+	System.out.println();
 	h.add(40);
-	//System.out.println(h.toString());
+	System.out.println(h.toString());
+	System.out.println();
 	h.add(9);
-	//System.out.println(h.toString());
+	System.out.println(h.toString());
+	System.out.println();
 	h.add(50);
 	System.out.println(h.toString());
+	System.out.println();
 	h.remove();
-	//System.out.println(h.toString());
+	System.out.println(h.toString());
+	System.out.println();
 	h.remove();
-	//System.out.println(h.toString());
+	System.out.println(h.toString());
+	System.out.println();
+
+	/*
+	MyHeap g = new MyHeap();
+	g.add(1);
+	System.out.println(g.toString());
+	g.add(3);
+	System.out.println(g.toString());
+	g.add(4);
+	System.out.println(g.toString());
+	g.add(0);
+	System.out.println(g.toString());
+	g.remove();
+	System.out.println(g.toString());
+	*/
     }
 }
--- 14RunningMedian/RunningMedianDriver.java
+++ 14RunningMedian/RunningMedianDriver.java
@@ -2,25 +2,35 @@ public class RunningMedianDriver{
     public static void main(String[]args){
 	RunningMedian m = new RunningMedian();
 	m.add(8);
-	System.out.println(m.getMedian());
+	//System.out.println(m.getMedian());
+	//System.out.println(m.toString());
 	m.add(1);
-	System.out.println(m.getMedian());
+	//System.out.println(m.getMedian());
+	//System.out.println(m.toString());
 	m.add(7);
-	System.out.println(m.getMedian());
+	//System.out.println(m.getMedian());
+	//System.out.println(m.toString());
 	m.add(3);
-	System.out.println(m.getMedian());
+	//System.out.println(m.getMedian());
+	//System.out.println(m.toString());
 	m.add(4);
 	System.out.println(m.getMedian());
+	System.out.println(m.toString());
+	System.out.println();
 	m.add(0);
 	System.out.println(m.getMedian());
-	m.add(2);
-	System.out.println(m.getMedian());
-	m.add(15);
-	System.out.println(m.getMedian());
-	m.add(8);
-	System.out.println(m.getMedian());
-	m.add(16);
-	System.out.println(m.getMedian());
 	System.out.println(m.toString());
+	m.add(2);
+	//System.out.println(m.getMedian());
+	//System.out.println(m.toString());
+	m.add(15);
+	//System.out.println(m.getMedian());
+	//System.out.println(m.toString());
+	m.add(8);
+	//System.out.println(m.getMedian());
+	//System.out.println(m.toString());
+	m.add(16);
+	//System.out.println(m.getMedian());
+	//System.out.println(m.toString());
     }
 }
